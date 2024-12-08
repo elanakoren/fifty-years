1a21c0f17f875259853112f8c2d1d2517872f7d7
Elana <elanakoren@gmail.com>
2024-12-07 17:34:50 -0800

    a little final styling

--- src/PersonPage.tsx
+++ src/PersonPage.tsx
@@ -6,23 +6,43 @@ export const PersonPage: React.FC<{
   neighbors: Person[];
 }> = ({ person, onClickBack, neighbors }) => {
   const renderBackButton = () => {
-    return <div onClick={onClickBack}>Back</div>;
+    return (
+      <div style={{ marginBottom: "10px" }}>
+        <button style={{borderRadius: '8px', backgroundColor: 'white'}} onClick={onClickBack}>Back</button>
+      </div>
+    );
   };
+
   const renderPersonDetails = () => {
-    return <>Name: {person.name}</>;
+    return (
+      <div>
+        <div>Name: {person.name}</div>
+        <div>Eye color: {person.eye_color}</div>
+        <div>Gender: {person.gender}</div>
+        <div>Height: {person.height}</div>
+        <div>Mass: {person.mass}</div>
+        <div>Skin color: {person.skin_color}</div>
+        <div>Birth year: {person.birth_year}</div>
+      </div>
+    );
   };
 
   const renderNeighbors = () => {
-    const neighborList = neighbors.map(neighbor => {
-      return (<div key={neighbor.name}>{neighbor.name}</div>)
-    })
+    const neighborList = neighbors.map((neighbor) => {
+      return <li key={neighbor.name}>{neighbor.name}</li>;
+    });
     return (
-      <div>
+      <div style={{ marginTop: "10px" }}>
         Planetary neighbors:
-        {neighborList.length > 0 ? neighborList : " No planetary neighbors"}
+        {neighborList.length > 0 ? (
+          <ul style={{marginTop: 0}}>{neighborList}</ul>
+        ) : (
+          " No planetary neighbors"
+        )}
       </div>
-    )
-  }
+    );
+  };
+
   return (
     <div>
       {renderBackButton()}
